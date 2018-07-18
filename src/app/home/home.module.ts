be197b09e6e5d47859a894b592a851549ffb47a8
aokrushko <alex.okrushko@gmail.com>
2018-07-17 22:19:06 -0400

    move stars to common

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import {MatCardModule} from '@angular/material';
-import { StarsModule } from '../stars/stars.module';
+import { StarsModule } from '../common/stars/stars.module';
 import { RouterModule } from '@angular/router';
 
 @NgModule({
--- src/app/product-details/product-details.module.ts
+++ src/app/product-details/product-details.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductDetailsComponent } from './product-details.component';
 import { MatCardModule, MatButtonModule } from '@angular/material';
-import { StarsModule } from '../stars/stars.module';
+import { StarsModule } from '../common/stars/stars.module';
 
 @NgModule({
   imports: [
