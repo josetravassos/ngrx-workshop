6a7c92db8596acb0459692cbaf75d7f59bfaa1be
aokrushko <alex.okrushko@gmail.com>
2018-07-21 20:54:21 -0400

    demo init effects

--- src/app/cart/actions.ts
+++ src/app/cart/actions.ts
@@ -7,4 +7,25 @@ export class AddItem implements Action {
   constructor(readonly itemId: string) {}
 }
 
-export type All = AddItem;
+export const FETCH_CART_ITEMS = '[cart] fetch items';
+export class FetchCartItems implements Action {
+  type: typeof FETCH_CART_ITEMS = FETCH_CART_ITEMS;
+}
+
+export const FETCH_CART_ITEMS_SUCCESS = '[cart] fetch items success';
+export class FetchCartItemsSuccess implements Action {
+  type: typeof FETCH_CART_ITEMS_SUCCESS = FETCH_CART_ITEMS_SUCCESS;
+
+  constructor(readonly itemIds: string[]) {}
+}
+
+export const FETCH_CART_ITEMS_ERROR = '[cart] fetch items error';
+export class FetchCartItemsError implements Action {
+  type: typeof FETCH_CART_ITEMS_ERROR = FETCH_CART_ITEMS_ERROR;
+}
+
+export type All =
+  | AddItem
+  | FetchCartItems
+  | FetchCartItemsSuccess
+  | FetchCartItemsError;
