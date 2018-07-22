d3e22b0bca312d090f9bd09323c022f6c8b187b5
aokrushko <alex.okrushko@gmail.com>
2018-07-22 12:14:13 -0400

    entities

--- src/app/cart/selectors.ts
+++ src/app/cart/selectors.ts
@@ -33,6 +33,10 @@ export const getCartProducts = createSelector<
   Product[],
   CartProduct[]
 >(getCartItemsIds, productSelectors.getProducts, (ids, products) => {
+  if (ids.length === 0 || products.length === 0) {
+    return [];
+  }
+
   // Reduce ids array to id:quantity Indexable
   const idsMap = ids.reduce((acc, id) => {
     const currentQuantity = acc[id] || 0;
