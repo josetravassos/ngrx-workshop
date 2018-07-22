ccbaf400fd49410415ad488b3df02a5d3d02dc11
aokrushko <alex.okrushko@gmail.com>
2018-07-21 20:25:22 -0400

    Add feature reducer and add Item

--- src/app/cart/cart.module.ts
+++ src/app/cart/cart.module.ts
@@ -1,9 +1,11 @@
-import {CommonModule} from '@angular/common';
-import {NgModule} from '@angular/core';
-import {MatButtonModule, MatIconModule} from '@angular/material';
-import {RouterModule} from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { MatButtonModule, MatIconModule } from '@angular/material';
+import { RouterModule } from '@angular/router';
 
-import {CartComponent} from './cart.component';
+import { CartComponent } from './cart.component';
+import { StoreModule } from '@ngrx/store';
+import { CART_FEATURE_KEY, reducer } from './reducer';
 
 @NgModule({
   imports: [
@@ -11,6 +13,9 @@ import {CartComponent} from './cart.component';
     MatIconModule,
     MatButtonModule,
     RouterModule,
+    StoreModule.forFeature(CART_FEATURE_KEY, {
+      cart: reducer,
+    }),
   ],
   declarations: [CartComponent],
   exports: [CartComponent],
